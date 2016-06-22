407c6ca29140deada0e4b37c899406ebc0884f51
aManNamedTed <davidamante17@yahoo.com>
2016-06-22 16:23:28 -0700

    adding all edits, encrypted java/c++ and new reverse string in java

---
+++ reverse_string.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class reverse_string
+{
+  public static void main(String[] args)
+  {
+    Scanner thePhrase = new Scanner(System.in);
+    String phrase;
+    int strLength;
+
+    System.out.println("Enter string that you want to reverse: ");
+
+    phrase = (thePhrase.next());
+
+    strLength = phrase.length();
+
+    System.out.println(strLength);
+
+    for(int i = 0; i < strLength; i++)
+    {
+      System.out.println(phrase(i));
+    }
+
+  }
+}
