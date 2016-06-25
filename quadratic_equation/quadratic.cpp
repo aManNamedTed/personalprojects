85917d59a5f6f8eda3e76750de625445901527bb
aManNamedTed <davidamante17@yahoo.com>
2016-06-25 00:54:42 -0700

    quadratic equation almost done. just need to create LaTeX write-to-file

---
+++ quadratic_equation/quadratic.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+  int a, b, c;
+
+  cout << "Quadratic Equation calculator to LaTeX v1.0.0" << endl;
+  cout << "Of the form: ax^2 + bx + c" << endl;
+  cout << "Enter the value of a: ";
+  cin >> a;
+  cout << "Enter the value of b: ";
+  cin >> b;
+  cout << "Enter the value of c: ";
+  cin >> c;
+  
+  return 0;
+}
