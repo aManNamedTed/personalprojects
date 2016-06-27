4bfec7ce913a4d918ccb77aa5a5afc76447ac099
aManNamedTed <davidamante17@yahoo.com>
2016-06-26 19:53:34 -0700

    latex file creation under way

--- quadratic_equation/quadratic.cpp
+++ quadratic_equation/quadratic.cpp
@@ -7,8 +7,12 @@ using std::endl;
 int main()
 {
   int a, b, c;
+  int font_size;
 
   cout << "Quadratic Equation calculator to LaTeX v1.0.0" << endl;
+  cout << "Enter the font pt size you want for your LaTeX doc: ";
+  cin >> font_size;
+
   cout << "Of the form: ax^2 + bx + c" << endl;
   cout << "Enter the value of a: ";
   cin >> a;
@@ -16,6 +20,13 @@ int main()
   cin >> b;
   cout << "Enter the value of c: ";
   cin >> c;
-  
+
+  cout << endl;
+  cout << "\\documentclass[" << font_size << "]{article}\n";
+  cout << "\\usepackage{fullpage}" << endl;
+  cout << "\\usepackage{amsfonts}" << endl;
+  cout << "\\usepackage{graphicx}" << endl;
+
+  cout << "\nbegin{document}" << endl;
   return 0;
 }
