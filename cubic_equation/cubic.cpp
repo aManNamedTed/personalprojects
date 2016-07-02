d74ad7e0b458c1f82ce7cf221df54e7629ef103e
David Amante <aManNamedTed@users.noreply.github.com>
2016-07-01 19:55:29 -0700

    Update cubic.cpp

--- cubic_equation/cubic.cpp
+++ cubic_equation/cubic.cpp
@@ -12,10 +12,32 @@ using std::cout;
 using std::endl;
 using std::pow;
 
+class Rational 
+{
+  public:
+    Rational();
+    
+    ostream& operator<<(const Rational r);
+  
+  private:
+    int _num;
+    int _denom;
+}
+
+Rational::Rational()
+{
+}
+
+ostream& operator<<(const Rational r)
+{
+  cout << r._num << " / " << r.denom;
+  
+}
+
 int main()
 {
-  int p = 0;
-  int q = 0;
+  int p = 6;
+  int q = 7;
   
   cout << "Enter your value of P: ";
   cin >> p;
@@ -23,5 +45,6 @@ int main()
   cout << "Enter your value of Q: ";
   cin >> q;
   
-  cout << "Delta = " << pow(
+  cout << "Delta = " << pow(q, 2) << " / " << 4 << " + " << pow(p, 3) << " / " << 27;
+  
 }
