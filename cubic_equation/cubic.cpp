110c22fed0a440e2dfcf5fdc9c5578bfe990d2d2
David Amante <aManNamedTed@users.noreply.github.com>
2016-07-01 19:49:00 -0700

    main.cpp started

---
+++ cubic_equation/cubic.cpp
@@ -0,0 +1,27 @@
+/*
+  Name: David Amante
+  Created: 1 July 2016
+  Project: Create a program that calculates the root of a cubic equation assuming delta is > 0.
+*/
+
+#include <iostream>
+#include <cmath>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::pow;
+
+int main()
+{
+  int p = 0;
+  int q = 0;
+  
+  cout << "Enter your value of P: ";
+  cin >> p;
+
+  cout << "Enter your value of Q: ";
+  cin >> q;
+  
+  cout << "Delta = " << pow(
+}
