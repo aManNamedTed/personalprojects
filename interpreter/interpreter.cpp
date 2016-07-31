3abdb1454a8eb544b2c851ec23101465de1e84f4
aManNamedTed <davidamante17@yahoo.com>
2016-07-30 20:28:20 -0700

    started interpreter

--- interpreter/interpreter.cpp
+++ interpreter/interpreter.cpp
@@ -13,12 +13,48 @@ int main()
 {
   ifstream in;
   string file_name;
+  char c;
 
-  cout << "Welcome to the /name/ interpreter." << endl;
-  cout << "Enter the name of the file you would like to interpret: ";
-  getline(cin, file_name);
+  //cout << "Welcome to the /name/ interpreter." << endl;
+  //cout << "Enter the name of the file you would like to interpret: ";
+  //getline(cin, file_name);
+  in.open("example.txt");
 
-  
+  //ignore single and multi-lined comments
+  while(in.get(c))
+  {
+    if(c == '@')
+    {
+      while(c != '\n')
+      {
+        in.get(c);
+      }
+    }
+    else if(c == '\n')
+    {
+      //effectively takes all spacing \n's out
+    }
+    else if(c == '#')
+    {
+      in.get(c);
+      if(c == ' ')
+      {
+
+      }
+      else
+      {
+        cout << "Your # declaration "
+      }
+
+
+    }
+    else
+    {
+      cout << c;
+    }
+  }
+
+  in.close();
 
   return 0;
 }
