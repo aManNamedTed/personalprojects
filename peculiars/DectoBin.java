56c5fb9263b9a09208e7c9ab513d6e60a8a4fe2e
aManNamedTed <davidamante17@yahoo.com>
2016-08-12 14:15:46 -0700

    decimal to binary program

---
+++ peculiars/DectoBin.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DectoBin {
+  public static void main(String args[])
+  {
+    Scanner input = new Scanner(System.in);
+    public int decimal;
+    System.out.print("Enter the decimal you want to convert to binary: ");
+    decimal = input.nextInt();
+    System.out.print(decimal);
+
+
+
+
+
+  }
+}
