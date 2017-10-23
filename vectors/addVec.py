f60d92922a540d477c76f2af86a40eba9c1abc4b
David Amante <davidamante@yahoo.com>
2017-10-22 20:50:00 -0700

    new vec

--- vectors/addVec.py
+++ vectors/addVec.py
@@ -2,22 +2,54 @@ import math
 
 def menu():
     print"Vector Calculation Menu"
-    print"1. Option 1"
-    print"2. Option 2"
+    print"1. Add Vectors"
+    print"2. Subtract Vectors"
     print"3. Option 3"
     print"4. Option 4"
     print"5. Exit"
 
-loop = True
+def addVectors():
+    print("Addition of two Vectors")
+    #U Vector
+    u1 = raw_input("Enter u1: ")
+    while not u1.isdigit():
+        u1 = raw_input("Float input only. Enter u1:");
 
+    u2 = raw_input("Enter vu: ")
+    while not u2.isdigit():
+        u1 = raw_input("Float input only. Enter u1:");
+
+    uVec = [u1, u2]
+
+    #V Vector
+    v1 = raw_input("Enter v1: ")
+    while not v1.isdigit():
+        v1 = raw_input("Float input only. Enter v1:");
+
+    v2 = raw_input("Enter v2: ")
+    while not v2.isdigit():
+        v1 = raw_input("Float input only. Enter v1:");
+
+    vVec = [v1, v2]
+
+    print("u = <%s,%s>, v = <%s,%s>" % (uVec[0], uVec[1], vVec[0], vVec[1]))
+
+    magn = math.sqrt(((float(uVec[1]) - float(uVec[0])))**2 + (float(vVec[1]) - float(vVec[0]))**2)
+    print(magn)
+
+    return
+
+
+loop = True
 while loop:
     menu()
-    choice = input("Enter your choice[1-5]: ")
-    while not choice.isdigit():
-        choice = input("Enter your choice[1-5]: ")
+
+    choice = "a"
+    while (choice.isdigit()):
+        choice = raw_input("Enter your choice[1-5]: ")
 
     if choice==1:
-        print "Menu 1 has been selected"
+        addVectors()
         ## You can add your code or functions here
     elif choice==2:
         print "Menu 2 has been selected"
@@ -35,38 +67,6 @@ while loop:
     else:
         # Any integer inputs other than values 1-5 we print an error message
         raw_input("Wrong option selection. Enter any key to try again..")
-
-print("Addition of two Vectors")
-#U Vector
-u1 = raw_input("Enter u1: ")
-while not u1.isdigit():
-    u1 = raw_input("Float input only. Enter u1:");
-
-u2 = raw_input("Enter vu: ")
-while not u2.isdigit():
-    u1 = raw_input("Float input only. Enter u1:");
-
-uVec = [u1, u2]
-
-#V Vector
-v1 = raw_input("Enter v1: ")
-while not v1.isdigit():
-    v1 = raw_input("Float input only. Enter v1:");
-
-v2 = raw_input("Enter v2: ")
-while not v2.isdigit():
-    v1 = raw_input("Float input only. Enter v1:");
-
-vVec = [v1, v2]
-
-print("u = <%s,%s>, v = <%s,%s>" % (uVec[0], uVec[1], vVec[0], vVec[1]))
-
-magn = math.sqrt(((float(uVec[1]) - float(uVec[0])))**2 + (float(vVec[1]) - float(vVec[0]))**2)
-print(magn)
-
-
-
-#print("This is %s and %s" % (v1, v2))
 
 
 """
---
+++ vectors/test.py
@@ -0,0 +1,8 @@
+i = 0
+
+v1 = "a"
+loop = False
+
+while (loop == v1.isdigit()):
+    v1 = input("%f: Enter your number (-1 to exit):" % i)
+    i += 1
