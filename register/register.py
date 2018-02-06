fea6f2442775b4a9ad377212ad1bad9d5eb8485a
David Amante <davidamante17@yahoo.com>
2018-02-05 16:52:45 -0800

    starting register

--- register/register.py
+++ register/register.py
@@ -1,1 +1,36 @@
-asdi
+import datetime
+
+def menu():
+    now = datetime.datetime.now()
+    print("David's Universal POS")
+    print("""
+        A: Sale
+        B: Return
+        C: Close Register
+        D: Duplicate Receipt
+        C: Post Void
+        E: No Sale
+        F: Employee Sale
+        G: Employee Return
+        H: Loans
+        I: Price Check
+        J: Inquires
+        K: Opening Balance
+        L: Closing Counts
+        M: Gift Receipt
+    """)
+    ans=input("Selection: ")
+    if ans == "1":
+        sale()
+        input("")
+    elif ans == "2":
+        subtractVectors()
+        input("")
+    else:
+        input("Wrong option selection. Enter any key to try again...")
+
+    return True
+
+loop = True
+while loop:
+    loop = menu()
