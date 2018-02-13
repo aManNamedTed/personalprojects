30c3c9921d7075e61f7c88728491f4d6b470fc8e
David Amante <davidamante17@yahoo.com>
2018-02-13 14:43:07 -0800

    r underway

--- r/stub.R
+++ r/stub.R
@@ -1,1 +1,11 @@
-# first r file
+labelledSentences = read.table(file=yelp_labelled,
+                               header=T, sep=" ", na.strings="'",
+                               stringAsFactors=F
+                              )
+
+sentenceData = labelledSentences[,c("SENTENCES", "SENTIMENT")]
+
+png(file="labelledSentenceData.png")
+plot(x=sentenceData$SENTENCES, y=sentenceData$SENTIMENT,
+     xlab="Sentences", ylab="Sentiment", main="Sentences and Sentiments"
+    )
