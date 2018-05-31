24518d6bcc48895a715b60d8fc43f35f1528548c
David Amante <davidamante17@yahoo.com>
2018-05-31 12:50:45 -0700

    newy

--- budget/budget.cpp
+++ budget/budget.cpp
@@ -1,75 +1,155 @@
-                <div className="section-desc">
-                    After watching <a href="https://www.youtube.com/channel/UCu3DXINXASlKe_HGwEZd4zg" target="_blank">gbay100's</a> video,
-                    <a href="https://www.youtube.com/watch?v=CJMUidRAk8E" target="_blank"> <i>What Every Young Person Should Know About Investing And Finance.</i></a>
-                    , I thought I would try to create a simple budget calculator.
-                    The goal is to make something that works today, and 
-                    then make it pretty over time. So for now, stage one of
-                    development is to make it just work, with numbers only. Then
-                    later I can make graphs and charts similar to the compound 
-                    interest website in the video.
+/*
+    Author: David Amante
+    Date: 5.29.18
+    Goal: Make a dynamic budget program that iterates through monthly cycles
+          until debts are paid off.
 
-                    For now here are the points that gbay touches on in order:
-                    <ol>
-                        <li>
-                            Save $1000 for a rainy day fund.
-                        </li>
-                        <li>
-                            Pay off your debt starting with the highest interest rate.
-                        </li>
-                        <li>
-                            Save up to 3-6 months of expenses.
-                        </li>
-                        <li>
-                            Utilize tax advantage accounts such as 401k or IRA.
-                        </li>
-                        <li>
-                            Save your age in bonds.
-                        </li>
-                    </ol>
-                    Feel free play around with the calculations below. For the
-                    sake of a disclaimer, I do not save any of the data that is
-                    input below. <br/><br/>
+    Example:
+        Monthly Expenses
+        ########|Income|Student Loan|Car Payment|Food|Gas
+        Month 1:  3200   68000-50     10000-50   145
+        Month 2:  3200
+        Month 3:  3200
+        ...
+        Month N:
 
-                    <form onSubmit={this.handleSubmit}>
-                        <label>
-                            Total:
-                            <input type="text" value={this.state.total}
-                                placeholder="Total"
-                                onChange={this.handleChange}
-                            />
-                        </label>
 
-                        <br/><br/>
-                        <input type="submit" value="Submit" />
-                    </form>
 
-            gain:
-                paychecks monthly = 3200/month
-                miscellaneous gains, negligible
-                gifts
-            loss:   
-                student loans = 50/month
-                car loans     = 50/month
-                credit cards  = ?? (let credit cards = 0, )
-                mortgage/rent = 1200/month,. if(mortgage > paycheck/(1/3), careful, over 33% paycheck)
-                stocks        = 10/month
 
-            trackTheseVars:
-                total 
-                totalMoneySaved = (budget->gain * .10 / month)
 
-                stepOne = save 1000 dollars for a rainy day fund
-                stepTwo = start paying off your debt where you have a higher interst rate first
-                stepThree = save up to 3-6 months of expenses
-                                    put this in terms of monthly amounts
-                                    1 month: 4000
-                                    2 months: 8000
-                                    3 months: 12000
-                                    etc
-                                define budget stuff
-                                    food
-                                    car
-                                    insurance
-                stepFour = take advantage of tax advantage accounts 401k, ira  
-                stepFive = save your age in bonds
-                    </div>
+
+    gain:
+        paychecks monthly = 3200/month
+        miscellaneous gains, negligible
+        gifts
+    loss:   
+        student loans = 35,000 total, 50/month until == 0
+        car loans     = 10,000 total, 50/month until == 0
+        mortgage/rent = 1200/month,. if(mortgage > paycheck/(1/3), careful, over 33% paycheck)
+        stocks        = 10/month
+
+    functions
+        Add Expense
+    trackTheseVars:
+        total 
+        totalMoneySaved = (budget->gain * .10 / month)
+
+        stepOne = save 1000 dollars for a rainy day fund
+        stepTwo = start paying off your debt where you have a higher interst rate first
+        stepThree = save up to 3-6 months of expenses
+                    put this in terms of monthly amounts
+                    1 month: 4000
+                    2 months: 8000
+                    3 months: 12000
+                    etc
+                define budget stuff
+                    food
+                    car
+                    insurance
+        stepFour = take advantage of tax advantage accounts 401k, ira  
+        stepFive = save your age in bonds
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class BudgetEntry {
+public:
+    double monthlyPayment;
+    double total;
+    string name;
+    bool isRecurrent;
+
+    BudgetEntry()
+    {
+        name = "";
+        monthlyPayment = 0;
+        total = 0;
+        isRecurrent = false;
+    }
+
+    BudgetEntry(string s, double mp, double t, bool r)
+    {
+        name = s;
+        monthlyPayment = mp;
+        total = t;
+        isRecurrent = r;
+    }
+};
+
+bool checkRepeat();
+void menu();
+double getMonthlyIncome();
+
+int main()
+{
+    bool repeat = true;
+    double monthlyIncome = getMonthlyIncome();
+
+    while(repeat)
+    {
+        // menu
+        menu();
+        // add an expense
+        // delete an expense
+        // show expenses until month count (< 0)
+        // show expenses until all debt paid
+        // export to latex
+        repeat = checkRepeat();
+    }
+    return 0;
+}
+
+
+
+//need to clean input
+double getMonthlyIncome() 
+{
+    double monthlyIncome = 0;
+    cout << "Enter your monthly income: ";
+    cin >> monthlyIncome;
+    return monthlyIncome;
+}
+
+void menu()
+{
+    cout << "        Budget Calculator Program        " << endl;
+    cout << "              Version 0.0.1              " << endl;
+    cout << "=========================================" << endl;
+    cout << "1: Add an expense                        " << endl;
+    cout << "2: Delete an expense                     " << endl;
+    cout << "3: Show expenses to ith month            " << endl;
+    cout << "4: Show expenses until all debt is paid  " << endl;
+    cout << "5: Export to LaTeX file                  " << endl;
+    cout << "6: Exit                                  " << endl;
+}
+
+bool checkRepeat()
+{
+    char repeatOption = 'n';
+    bool repeat = false;
+
+    cout << "Would you like to edit your data? (y/n)" << endl;
+    cin >> repeatOption;
+
+    while(repeatOption != 'N' && repeatOption != 'n' &&
+          repeatOption != 'Y' && repeatOption != 'y')
+    {
+        cout << "Would you like to edit your data? (y/n)" << endl;
+        cin >> repeatOption;
+    }
+
+    if(repeatOption == 'Y' || repeatOption == 'y')
+    {
+        repeat = true;
+    }
+    else if(repeatOption == 'N' || repeatOption == 'n')
+    {
+        repeat = false;
+    }
+
+    return repeat;
+}
--- budget/readme.md
+++ budget/readme.md
@@ -1,3 +1,8 @@
+# Usage
+
+You can download and compile the code by compiling budget.cpp with your 
+favorite C++ compiler. 
+
 # Background
 After watching gbay100's video, "What Every Young Person Should Know About
 Investing And Finance.", I thought I would try to create a simple budget 
@@ -7,8 +12,5 @@ For now here are the points that gbay touches on in order:
 1. Save $1000 for a rainy day fund.
 2. Pay off your debt starting with the highest interest rate.
 3. Save up to 3-6 months of expenses.
-Utilize tax advantage accounts such as 401k or IRA.
-Save your age in bonds.
-Feel free play around with the calculations below. For the
-sake of a disclaimer, I do not save any of the data that is
-input below. <br/><br/>
+4. Utilize tax advantage accounts such as 401k or IRA.
+5. Save your age in bonds.
