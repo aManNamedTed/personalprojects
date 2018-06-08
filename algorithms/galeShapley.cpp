5fad3e3a3da734d7771ce1b6181d233aaeedb231
David Amante <davidamante17@yahoo.com>
2018-06-07 21:14:05 -0700

    gale shap

---
+++ algorithms/galeShapley.cpp
@@ -0,0 +1,70 @@
+
+/*
+    As per the numberphile video
+
+    Charlotte | Elizabeth | Jane | Lydia
+    ====================================
+    Bingley   | Wickham   | Bing | Bing
+    Darcy     | Darcy     | Wick | Wick
+    Collins   | Bingley   | Darc | Darc
+    Wickham   | Collins   | Coll | Coll
+
+
+    Bingley | Collins | Darcy | Wickham
+    ===================================
+    Jane    | Jane    | Eliz  | Lydia
+    Eliz    | Eliz    | Jane  | Jane
+    Lydia   | Lydia   | Char  | Eliz
+    Char    | Char    | Lydia | Char
+
+    [] implement gale-shapley
+    [] implement a class to solve gale-shapley
+*/
+
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    //make women prefs, array per woman
+    char charlotte[4] = {'B', 'D', 'C', 'W'};
+    char elizabeth[4] = {'W', 'D', 'B', 'C'};
+    char jane[4] =      {'B', 'W', 'D', 'C'};
+    char lydia[4] =     {'B', 'W', 'D', 'C'};
+
+    //make men prefs, array per man
+    char bingley[4] = {'J', 'E', 'L', 'C'};
+    char collins[4] = {'J', 'E', 'L', 'C'};
+    char darcy[4] =   {'E', 'J', 'C', 'L'};
+    char wickham[4] = {'L', 'J', 'E', 'C'};
+
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            
+        }
+    }
+
+    vector<string> matches;
+    matches.clear();
+    string match = "";
+    matches.push_back{match};
+    return 0;
+}    
+/*
+    char womenPreferences[4][4] = {{'B', 'W', 'B', 'B'},
+                                   {'D', 'D', 'W', 'W'},
+                                   {'C', 'B', 'D', 'D'},
+                                   {'W', 'C', 'C', 'C'}
+    };
+
+    //make men prefs, columns 
+    char menPreferences[4][4] = {{'J', 'J', 'E', 'L'},
+                                 {'E', 'E', 'J', 'J'},
+                                 {'L', 'L', 'C', 'E'},
+                                 {'C', 'C', 'L', 'C'}
+    };
+    */
